738e04f51384b33e10b057092961f15cde508bdb
jsonbsd <jsonbsd@gmail.com>
2025-06-21 12:02:02 +0900

    feat: .gitignore fixed

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,26 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1750407930',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
