e7e81933d85b329e6c6f796396338915f251de2e
jsonbsd <jsonbsd@gmail.com>
2025-06-22 16:01:27 +0900

    feat: CV added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,12 +1,14 @@
 const swconf = {
   
-    cacheName: 'chirpy-1750407930',resources: [
+    cacheName: 'chirpy-1750575666',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
         '/categories/',
       
         '/about/',
+      
+        '/assets/file/CV.pdf',
       
 
       
---
+++ _tabs/CV.md
@@ -0,0 +1,8 @@
+---
+layout: none
+icon: fas fa-archive
+order: 3
+permalink: /assets/file/CV.pdf
+redirect_to: /assets/file/CV.pdf
+url: /assets/file/CV.pdf
+---
